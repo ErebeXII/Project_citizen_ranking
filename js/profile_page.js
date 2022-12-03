9732d239227798e447bb8a61e92f40bf9064b24a
Timothé <timothe.perier2403@gmail.com>
2022-12-03 15:14:38 +0800

    profile page modify personnal data

---
+++ js/profile_page.js
@@ -0,0 +1,42 @@
+let edit_data = false;
+
+function editPersonalData(){
+    let address = document.getElementById("current_address");
+    let mail = document.getElementById("mail_data");
+    let phone = document.getElementById("phone_data");
+    let edit = document.getElementById("edit_btn");
+
+    if (!edit_data){
+        edit_data = true;
+        edit.innerHTML = "&#9989;";
+
+        editCSSChanges(address);
+        editCSSChanges(mail);
+        editCSSChanges(phone);
+
+    }
+    else {
+        edit_data = false;
+        edit.innerHTML = "&#9999;&#65039;";
+
+        editCSSChanges(address, true);
+        editCSSChanges(mail, true);
+        editCSSChanges(phone, true);
+
+    }
+
+}
+
+function editCSSChanges(elmnt, reset){
+
+    if (reset !== true){
+        elmnt.style.backgroundColor = "rgb(0, 255, 46)";
+        elmnt.style.padding = ".5em";
+        elmnt.contentEditable = true;
+    }
+    else {
+        elmnt.style.backgroundColor = "transparent";
+        elmnt.style.padding = "0";
+        elmnt.contentEditable = false;
+    }
+}
